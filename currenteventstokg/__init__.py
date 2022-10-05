7546c7e3750ea9c64cec24df34403d4c827eda64
Lars <lars@lars-michaelis.de>
2022-10-05 13:24:47 +0200

    added dir var for main package

---
+++ currenteventstokg/__init__.py
@@ -0,0 +1,7 @@
+# Copyright: (c) 2022, Lars Michaelis
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from pathlib import Path
+from os.path import abspath, split
+
+currenteventstokg_module_dir = Path(split(abspath(__file__))[0])
